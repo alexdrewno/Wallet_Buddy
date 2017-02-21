dc9d93d0dde61837e58d0d82a5ef9f036bb03886
Alex Drewno <student@adrewno1.local>
2017-02-21 09:33:19 -0600

    Initial Project

---
+++ Wallet_Buddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wallet_Buddy
+//
+//  Created by adrewno1 on 2/21/17.
+//  Copyright © 2017 adrewno1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
