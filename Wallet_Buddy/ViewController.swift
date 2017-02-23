caf33a944be8ace7396b913f9d6a2a5a800bdd38
Alex Drewno <student@adrewno1.local>
2017-02-23 09:40:59 -0600

    Working

--- Wallet_Buddy/ViewController.swift
+++ Wallet_Buddy/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         performSegue(withIdentifier: "authorized", sender: self)
     }
 
